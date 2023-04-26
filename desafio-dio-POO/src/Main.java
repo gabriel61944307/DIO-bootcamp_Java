d0d7991563342be656fea6a102c694b96931884a
gabriel61944307 <gabrielrodriguesmalaquias1111@gmail.com>
2023-04-26 01:55:25 -0300

    Commit final com teste das classes.

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -13,8 +11,34 @@ public class Main {
 
         Conteudo mentoria1 = new Mentoria("Banco PAN", "Descricao mentoria.", LocalDate.now());
 
-        System.out.println(curso1);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java");
+        bootcamp.setDescricao("descricao Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devGabriel = new Dev();
+        devGabriel.setNome("Gabriel");
+        devGabriel.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Gabriel: " + devGabriel.getConteudosInscritos());
+        devGabriel.progredir();
+        devGabriel.progredir();
+        System.out.println("Conteudos inscritos Gabriel: " + devGabriel.getConteudosInscritos());
+        System.out.println("Conteudos inscritos Gabriel: " + devGabriel.getConteudosConcluidos());
+        System.out.println("XP: " + devGabriel.calcularTotalXp());
+
+        Dev devLucas = new Dev();
+        devLucas.setNome("Lucas");
+        devLucas.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Lucas: " + devLucas.getConteudosInscritos());
+        devLucas.progredir();
+        System.out.println("Conteudos inscritos Lucas: " + devLucas.getConteudosInscritos());
+        System.out.println("Conteudos inscritos Lucas: " + devLucas.getConteudosConcluidos());
+        System.out.println("XP: " + devLucas.calcularTotalXp());
+
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria1);
+        System.out.println(mentoria1);*/
     }
 }
